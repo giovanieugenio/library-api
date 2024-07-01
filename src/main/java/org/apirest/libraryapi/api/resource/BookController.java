b76343911ef2e56d62628e7f1a3e7a08671e9a58
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-01 15:09:38 -0300

    implementado metodos no controller

--- src/main/java/org/apirest/libraryapi/api/resource/BookController.java
+++ src/main/java/org/apirest/libraryapi/api/resource/BookController.java
@@ -7,16 +7,20 @@ import org.apirest.libraryapi.exception.BusinessException;
 import org.apirest.libraryapi.model.entity.Book;
 import org.apirest.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/api/books")
@@ -37,6 +41,30 @@ public class BookController {
         Book entity = modelMapper.map(dto, Book.class);
         entity = bookService.save(entity);
         return modelMapper.map(entity, BookDto.class);
+    }
+
+    @GetMapping("/{id}")
+    public BookDto getBook(@PathVariable Long id){
+        return bookService.getBookById(id).map(book -> modelMapper.map(book, BookDto.class)).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        Book book = bookService.getBookById(id).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        bookService.delete(book);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto update(@PathVariable Long id, BookDto dto){
+        Book book = bookService.getBookById(id).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        book.setTitle(dto.getTitle());
+        book.setAuthor(dto.getAuthor());
+        return modelMapper.map(bookService.update(book), BookDto.class);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
