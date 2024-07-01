cf77699bfe0814cb1dd0f66ef7918436f64b9e6e
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-01 15:12:36 -0300

    declarado os metodos para implementação

--- src/main/java/org/apirest/libraryapi/service/BookService.java
+++ src/main/java/org/apirest/libraryapi/service/BookService.java
@@ -3,8 +3,16 @@ package org.apirest.libraryapi.service;
 import org.apirest.libraryapi.model.entity.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface BookService {
 
     Book save(Book book);
+
+    Optional<Book> getBookById(Long id);
+
+    void delete(Book book);
+
+    Book update(Book book);
 }
