eae718cc333b56991dabe5c2da1df24832f6754c
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-04 22:54:38 -0300

    adicionado dependencia para gerar arquivo war

--- src/main/java/org/apirest/libraryapi/LibraryApiApplication.java
+++ src/main/java/org/apirest/libraryapi/LibraryApiApplication.java
@@ -3,6 +3,7 @@ package org.apirest.libraryapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @Configuration
 @SpringBootApplication
-public class LibraryApiApplication {
+public class LibraryApiApplication extends SpringBootServletInitializer {
 
     @Bean
     public ModelMapper modelMapper(){
